93bae2d2597a8d6dbaf2d276abfad30cd478fea8
Xavier Shay <xavier@rhnh.net>
2014-05-04 11:40:43 -0700

    Allow host and port to be set with environment variables.

--- erg.go
+++ erg.go
@@ -7,20 +7,34 @@ import (
 	"net/url"
 	"os"
 	"sort"
+	"strconv"
 
 	"github.com/xaviershay/grange"
 
 	goopt "github.com/droundy/goopt"
 )
 
-var port = goopt.Int([]string{"-p", "--port"}, 8080, "Port to connect to")
-var host = goopt.String([]string{"-h", "--host"}, "localhost", "Host to connect to")
+var port = goopt.Int([]string{"-p", "--port"}, 8080, "Port to connect to. Can also be set with RANGE_PORT environment variable.")
+var host = goopt.String([]string{"-h", "--host"}, "localhost", "Host to connect to. Can also be set with RANGE_HOST environment variable.")
 var expand = goopt.Flag([]string{"-e", "--expand"}, []string{"--no-expand"},
 	"Do not compress results", "Compress results (default)")
 var noSortResult = goopt.Flag([]string{"--no-sort"}, []string{"-s", "--sort"},
 	"Do not sort results. Only relevant with --expand option.", "Sort results (default)")
 
 func main() {
+	if envHost := os.Getenv("RANGE_HOST"); len(envHost) > 0 {
+		*host = envHost
+	}
+
+	if envPort := os.Getenv("RANGE_PORT"); len(envPort) > 0 {
+		x, err := strconv.Atoi(envPort)
+		if err == nil {
+			*port = x
+		} else {
+			fmt.Fprintf(os.Stderr, "Invalid port in RANGE_PORT: %s\n", envPort)
+			os.Exit(1)
+		}
+	}
 	goopt.Parse(nil)
 
 	var query string
@@ -56,9 +70,9 @@ func main() {
 			for node := range result.Iter() {
 				strResult = append(strResult, node.(string))
 			}
-      if !*noSortResult {
-        sort.Strings(strResult)
-      }
+			if !*noSortResult {
+				sort.Strings(strResult)
+			}
 			for _, node := range strResult {
 				fmt.Println(node)
 			}
